3eccc4f6515eb6c72aa8fcfd0e67d788d026afd8
JaruguAakanksha <146044330+JaruguAakanksha@users.noreply.github.com>
2023-10-03 01:00:06 +0530

    Add files via upload

---
+++ Updated LetterGrade.java
@@ -0,0 +1,54 @@
+//Author- Aakanksha Jarugu
+
+import java.util.Scanner;
+public class LetterCount
+{
+	public static void main(String[] args) 
+	{
+		String letterGrade;
+
+        System.out.println(" Please enter your marks: ");
+
+        Scanner scan =  new Scanner(System.in);
+
+        int grade = scan.nextInt();
+
+        if (grade >= 95 )
+            letterGrade = "A+";
+
+        else if(grade >= 90)
+            letterGrade = "A";
+
+        else if(grade >= 85)
+            letterGrade = "B+";
+
+        else if(grade >= 80)
+            letterGrade = "B";
+            
+        else if(grade >= 75)
+            letterGrade = "C+";
+            
+        else if(grade >= 70)
+            letterGrade = "C";
+            
+        else if(grade >= 65)
+            letterGrade = "D+";
+            
+        else if(grade >= 60)
+            letterGrade = "D";
+
+        else
+            letterGrade = "F";
+
+       System.out.println("The LetterGrade for " + grade + " is " + letterGrade);
+
+       scan.close();
+
+    }
+
+}
+
+Output:
+Please enter your marks: 
+91
+The LetterGrade for 91 is A
